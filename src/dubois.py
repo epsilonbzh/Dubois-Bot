5e3a3f362d2f62106288f3b5d30f084975d8af79
Paul Coignac <coignac.e2001154@etud.univ-ubs.fr>
2022-10-19 11:37:07 +0200

    Dubois a une chance sur 100 de se marrer sans raison après le post d'un message

--- src/dubois.py
+++ src/dubois.py
@@ -1,17 +1,28 @@
 import asyncio
+import random
 
 import discord
 from discord.ext import commands
 
-token = open("token.txt","r").readlines()[0]
+token = open("token.txt", "r").readlines()[0]
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
 
+
 async def main():
     async with bot:
+        @bot.event
+        async def on_message(message):
+            choice = random.randint(0, 50)
+            if choice == 1 :
+                laughts = open("../data/laughts.txt", "r").readlines()
+                idx = random.randint(0, len(laughts) - 1)
+                await message.channel.send(laughts[idx].upper())
+
         await bot.load_extension('commands')
         await bot.start(token)
+
 
 asyncio.run(main())

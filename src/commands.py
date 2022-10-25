b97e3a098c85eef6169b9b72bd53ad00d41d4b65
pagaro <dev.pagaro@gmail.com>
2022-10-25 10:34:04 +0200

    add autosigne by hour

--- src/commands.py
+++ src/commands.py
@@ -1,9 +1,7 @@
 import random
-import json
-
 import discord
 from discord.ext import commands
-from UserSWS import UserSWS
+from src.signature import signmeC, signallC
 
 
 class Commands(commands.Cog):
@@ -28,59 +26,12 @@ class Commands(commands.Cog):
         await ctx.send(quotes[choice])
 
     @commands.command()
-    async def signe(self, ctx):
-        try:
-            await ctx.send("Signing in progress")
-            f = open("../data/signature.json")
-
-            data = json.load(f)
-            for etablisement in data:
-                for user in etablisement["users"]:
-                    res = "<@" + user["id"] + ">" + " error unable to sign"
-                    try:
-                        if user["autosign"]:
-                            userSWS = UserSWS(codeEtablisement=etablisement["codeEtablisement"],
-                                              codeIdentifiant=user["code_identifiant"],
-                                              codePin=user["code_pin"])
-
-                            if userSWS.hasSigned():
-                                res = res = "<@" + user["id"] + ">" + " signature send"
-                    except Exception as err:
-                        print(err)
-                        await ctx.send(err)
-                    print(res)
-                    await ctx.send(res)
-
-            await ctx.send("all signatures done")
-        except Exception as err:
-            await ctx.send(err)
+    async def signall(self, ctx):
+        await signallC(ctx)
 
     @commands.command()
-    async def jesigne(self, ctx):
-        try:
-            f = open("../data/signature.json")
-            idUser = str(ctx.message.author.id)
-            data = json.load(f)
-            userSWS = -1
-            res = "<@" + idUser + ">"
-            for etablisement in data:
-                for user in etablisement["users"]:
-                    if idUser == user["id"]:
-                        userSWS = UserSWS(codeEtablisement=etablisement["codeEtablisement"],
-                                          codeIdentifiant=user["code_identifiant"],
-                                          codePin=user["code_pin"])
-
-                if userSWS == -1:
-                    res += str(ctx.message.author) + " not in database, id : " + idUser
-                else:
-                    if userSWS.hasSigned():
-                        res += " signed"
-                    else:
-                        res += " error unable to sign"
-                await ctx.send(res)
-
-        except Exception as err:
-            await ctx.send(err)
+    async def signme(self, ctx):
+        await signmeC(ctx)
 
 
 async def setup(bot):
--- src/dubois.py
+++ src/dubois.py
@@ -1,19 +1,48 @@
 import asyncio
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from datetime import datetime, time
+from src.signature import signallC
 
 token = open("token.txt", "r").readlines()[0]
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents, help_command=None)
+target_channel_id = 1032901511132487713
+
+hourSign = [time.fromisoformat('00:00:00')]
+# hourSign = [time.fromisoformat('08:21:00')]
+weekday = datetime.today().weekday()
+
+if {0, 1, 2, 4}.__contains__(weekday):
+    hourSign.append(time.fromisoformat('06:00:00'))
+    hourSign.append(time.fromisoformat('07:45:00'))
+    hourSign.append(time.fromisoformat('09:30:00'))
+if {0, 1, 2, 3, 4}.__contains__(weekday):
+    hourSign.append(time.fromisoformat('12:00:00'))
+    hourSign.append(time.fromisoformat('13:45:00'))
+
+@tasks.loop(time=hourSign)
+# @tasks.loop(seconds=1)
+async def autosign():
+    message_channel = bot.get_channel(target_channel_id)
+    await signallC(message_channel)
+
+
+@autosign.before_loop
+async def before():
+    await bot.wait_until_ready()
+    message_channel = bot.get_channel(target_channel_id)
+    await message_channel.send("autosign starting")
 
 
 async def main():
     async with bot:
         await bot.load_extension('events')
         await bot.load_extension('commands')
+        autosign.start()
         await bot.start(token)
 
 
